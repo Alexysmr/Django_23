b87ec4a69dcf3fc4dcab3d739e2f56744bd765a0
Alexxy <alexxsmr@yandex.ru>
2026-04-30 11:58:02 +0400

    Update:readme.md, .gitignore  catalog\constant.py

--- catalog/constant.py
+++ catalog/constant.py
@@ -9,8 +9,8 @@ MAX_IMAGE_SIZE_BYTES = 5242880  # Максимальный размер загр
 TYPE_OF_IMAGE = ['JPEG', 'PNG']  # Допустимый формат загружаемого файла изображения в карточку товара
 
 FIELD_ATTRIBUTES = {  # Настройка атрибутов в соответствии типам полей
-    models.CharField: {'class': 'form-control', 'placeholder': 'text'},
-    models.EmailField: {'class': 'form-control', 'type': 'email', 'placeholder': 'email'},
+    models.CharField: {'class': 'form-control', 'placeholder': 'текст'},
+    models.EmailField: {'class': 'form-control', 'type': 'email', 'placeholder': 'почта'},
     models.DateTimeField: {'class': 'form-control', 'type': 'datetime-local'},
     models.ForeignKey: {'class': 'form-select'},
     models.BooleanField: {'class': 'form-check-input'},
